b6ea3cf8fc13fd4da5be5a8674840ba0156b414d
Granovskiy Uriy <silaveri777@gmail.net>
2024-05-27 02:46:09 +0300

    up-auth-7

--- app.js
+++ app.js
@@ -3,33 +3,35 @@ import morgan from "morgan";
 import cors from "cors";
 import contactsRouter from "./routes/contactsRouter.js";
 import authRouter from "./routes/authRouter.js";
-import usersRoutes from "./routes/usersRouter.js";
 import mongoose from "mongoose";
-import path from "path";
+import authTokenUsePassport from "./middleware/authTokenUsePassport.js";
+import authToken from "./middleware/authToken.js";
+import usersRoutes from "./routes/usersRouter.js";
 import "dotenv/config";
+import path from "path";
+
 
 const app = express();
-app.use(morgan("tiny"));
+
+app.use(morgan("tiny")); //
 app.use(cors());
 app.use(express.json());
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/avatars", express.static(path.resolve("public/avatars")));
-app.use("/api/contacts", contactsRouter);
-app.use("/api/users", authRouter);
-app.use("/users", authToken, usersRoutes);
 
+app.use("/avatars", express.static(path.resolve("public/avatars")));
+
+app.use("/api/contacts", authTokenUsePassport, contactsRouter);
+app.use("/api/users", authRouter);
+// Додай можливість поновлення аватарки, створивши ендпоінт /users/avatars
+app.use("/users", authToken, usersRoutes);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
-
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
 
 const DB_URI = process.env.DB_URI;
 
@@ -40,14 +42,11 @@ mongoose
 
     const PORT = process.env.PORT || 3000;
 
-  
     app.listen(PORT, () => {
       console.log(`Server is running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
-  
     console.error("Database connection error:", error);
-  
     process.exit(1);
   });
