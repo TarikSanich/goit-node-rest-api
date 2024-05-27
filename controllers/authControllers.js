ccdabd5ebe5cde7c999238e3965e4eb0ba3f84b7
Granovskiy Uriy <silaveri777@gmail.net>
2024-05-27 03:48:46 +0300

    up12

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,11 @@
 import HttpError from "../helpers/HttpError.js";
-import { registerUserSchema } from "../schemas/usersSchemas.js";
+import { registerUserSchema, loginUserSchema } from "../schemas/usersSchemas.js";
 import gravatar from 'gravatar';
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+const { JWT_SECRET } = process.env;
 
 export const registerUser = async (req, res, next) => {
   const { error } = registerUserSchema.validate(req.body);
@@ -13,28 +14,27 @@ export const registerUser = async (req, res, next) => {
   }
 
   try {
-    const existUser = await User.findOne({ email: req.body.email });
+    const { email, password } = req.body;
+    const existUser = await User.findOne({ email });
 
-    if (existUser !== null) {
+    if (existUser) {
       return res.status(409).send({ message: "Email in use" });
     }
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email, { s: "250", d: "retro" }, true);
 
-    const avatar = gravatar.url(email, { s: "250", d: "retro" }, true);
-
-
-    await User.create({
-      email: req.body.email,
+    const newUser = await User.create({
+      email,
       password: hashedPassword,
-      avatarURL: avatar,
+      avatarURL,
     });
 
     res.status(201).send({
       user: {
-        email: req.body.email,
-        subscription: "starter",
-        avatarURL: avatarURL,
+        email: newUser.email,
+        subscription: newUser.subscription,
+        avatarURL: newUser.avatarURL,
       },
     });
   } catch (error) {
@@ -43,63 +43,57 @@ export const registerUser = async (req, res, next) => {
 };
 
 export const loginUser = async (req, res, next) => {
-  const { error } = registerUserSchema.validate(req.body);
+  const { error } = loginUserSchema.validate(req.body);
   if (error) {
     return res.status(400).send({ message: error.message });
   }
+
   try {
-    const existUser = await User.findOne({ email: req.body.email });
-    if (existUser === null) {
-      console.log("Email is wrong");
+    const { email, password } = req.body;
+    const existUser = await User.findOne({ email });
+
+    if (!existUser) {
       return res.status(401).send({ message: "Email or password is wrong" });
     }
 
-    const isMatch = await bcrypt.compare(req.body.password, existUser.password);
-    if (isMatch === false) {
-      console.log("Password is wrong");
+    const isMatch = await bcrypt.compare(password, existUser.password);
+    if (!isMatch) {
       return res.status(401).send({ message: "Email or password is wrong" });
     }
 
-    
-    const payload = {
-      id: existUser.id,
-    };
-
-    const secret = process.env.JWT_SECRET;
-
-    const token = jwt.sign(payload, secret, { expiresIn: "23h" });
+    const payload = { id: existUser._id };
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
 
     existUser.token = token;
     await existUser.save();
 
     res.status(200).send({
-      token: token,
+      token,
       user: {
-        email: req.body.email,
-        subscription: "starter",
+        email: existUser.email,
+        subscription: existUser.subscription,
+        avatarURL: existUser.avatarURL,
       },
     });
-    next();
   } catch (error) {
     next(error);
   }
 };
 
-
 export const logoutUser = async (req, res, next) => {
   try {
-    const userId = req.user._id; 
+    const userId = req.user._id;
 
     const user = await User.findById(userId);
 
-    if (user === null) {
+    if (!user) {
       return res.status(401).send({ message: "Not authorized" });
     }
 
     user.token = null;
     await user.save();
 
-    return res.status(204).send("No Content");
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
@@ -107,20 +101,23 @@ export const logoutUser = async (req, res, next) => {
 
 export const getCurrentUser = async (req, res, next) => {
   try {
-    const userId = req.user._id; 
+    const userId = req.user._id;
 
     const user = await User.findById(userId);
 
-    if (user === null) {
+    if (!user) {
       return res.status(401).send({ message: "Not authorized" });
     }
-    const result = { email: user.email, subscription: user.subscription };
-    return res.status(200).send(result);
+
+    res.status(200).send({
+      email: user.email,
+      subscription: user.subscription,
+      avatarURL: user.avatarURL,
+    });
   } catch (error) {
     next(error);
   }
 };
-
 
 export const updateSubscription = async (req, res, next) => {
   const validSubscriptions = ["starter", "pro", "business"];
@@ -131,7 +128,7 @@ export const updateSubscription = async (req, res, next) => {
   }
 
   try {
-    const userId = req.user.id;
+    const userId = req.user._id;
 
     const user = await User.findById(userId);
 
@@ -142,12 +139,14 @@ export const updateSubscription = async (req, res, next) => {
     user.subscription = subscription;
     await user.save();
 
-    res
-      .status(200)
-      .json({
-        message: "Subscription updated successfully",
-        user: { email: user.email, subscription: user.subscription },
-      });
+    res.status(200).json({
+      message: "Subscription updated successfully",
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+        avatarURL: user.avatarURL,
+      },
+    });
   } catch (error) {
     next(error);
   }
